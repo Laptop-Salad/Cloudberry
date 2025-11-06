64243a24038c5fd1e9bed6de945d779987c11110
Amanda Wallis <80591698+Laptop-Salad@users.noreply.github.com>
2025-11-06 22:53:10 +0000

    feat(admin.user): view all users

--- app/Livewire/Admin/Users/Index.php
+++ app/Livewire/Admin/Users/Index.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire\Admin\Users;
 
+use App\Models\User;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[On(['refresh-users'])]
 class Index extends Component
 {
+    use WithPagination;
+
+    #[Computed]
+    public function users() {
+        return User::query()
+            ->orderBy('name')
+            ->paginate(15);
+    }
+
     public function render()
     {
         return view('livewire.admin.users.index');
