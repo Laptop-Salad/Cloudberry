420d537d7f60b5f8fc1acceb9fb2f384d11143ab
Amanda Wallis <80591698+Laptop-Salad@users.noreply.github.com>
2025-11-07 00:38:38 +0000

    feat(delivery-companies): show all delivery companies

--- app/Livewire/DeliveryCompanies/Index.php
+++ app/Livewire/DeliveryCompanies/Index.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\DeliveryCompanies;
 
+use App\Models\DeliveryCompany;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[On(['refresh-credit-companies'])]
 class Index extends Component
 {
+    use WithPagination;
+
+    #[Computed]
+    public function deliveryCompanies() {
+        return DeliveryCompany::query()
+            ->orderBy('name')
+            ->paginate(15);
+    }
+
     public function render()
     {
         return view('livewire.delivery-companies.index');
     }
 }
+
--- resources/views/livewire/delivery-companies/index.blade.php
+++ resources/views/livewire/delivery-companies/index.blade.php
@@ -1,3 +1,49 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    <x-page-heading
+        title="Delivery Companies"
+        subtitle="View and manage all delivery companies"
+    >
+        <livewire:credit-companies.create />
+    </x-page-heading>
+
+    <table class="basic-table">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Location</th>
+            <th>COD</th>
+            <th>Annual Obligations</th>
+            <th>Weekly Obligations</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        @foreach($this->delivery_companies as $delivery_company)
+            <tr>
+                <td class="font-medium">
+                    {{$delivery_company->name}}
+                    <p class="text-sm font-normal">
+                        {{$delivery_company->type}}
+                        •
+                        {{$delivery_company->buffer_tank_size}} Tonnes Buffer
+                        •
+                        {{$delivery_company->creditCompany->name}}
+                    </p>
+                </td>
+                <td>{{$delivery_company->location}}</td>
+                <td>{{$delivery_company->cod}}</td>
+                <td>{{$delivery_company->annual_min_obligation}} - {{$delivery_company->annual_max_obligation}}</td>
+                <td>{{$delivery_company->weekly_min}} - {{$delivery_company->weekly_max}}</td>
+                <td>
+                    <flux:button
+                        icon="cog-6-tooth"
+                        wire:click="$dispatch('edit-production-site', { 'production-site' : {{$delivery_company->id}}})"
+                    >
+                        Manage
+                    </flux:button>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 </div>
