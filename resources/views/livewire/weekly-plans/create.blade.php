fb9b818aa376903ec1245ceb19275a169bcaf6b2
Amanda Wallis <80591698+Laptop-Salad@users.noreply.github.com>
2025-11-17 22:45:54 +0000

    fix generate plan typo

--- resources/views/livewire/weekly-plans/create.blade.php
+++ resources/views/livewire/weekly-plans/create.blade.php
@@ -1,10 +1,10 @@
 <div>
     <flux:modal.trigger wire:click="showForm">
-        <flux:button icon="truck">Create Event</flux:button>
+        <flux:button icon="truck">Generate Plan</flux:button>
     </flux:modal.trigger>
 
     <flux:modal wire:model="show" class="w-6xl">
-        <flux:heading size="lg">Create Event</flux:heading>
+        <flux:heading size="lg">Generate Plan</flux:heading>
 
         <form wire:submit="save" class="mt-6 space-y-6">
             <flux:input
