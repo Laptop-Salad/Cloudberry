c4384f5e451cd1e16564b55247085be98bd1181c
Amanda Wallis <80591698+Laptop-Salad@users.noreply.github.com>
2025-11-17 11:23:38 +0000

    view a site

--- resources/views/livewire/production-sites/show.blade.php
+++ resources/views/livewire/production-sites/show.blade.php
@@ -1,3 +1,24 @@
 <div>
     <x-production-sites.header />
+
+    {{-- NOTE: Not displaying shutdown periods here because we have the events tab --}}
+    {{-- NOTE: Name displayed in header --}}
+
+    <p>Location</p>
+    <p>{{$this->production_site->location}}</p>
+
+    <p>Type</p>
+    <p>{{$this->production_site->type}}</p>
+
+    <p>Status</p>
+    <p>{{$this->production_site->system_operating_status}}</p>
+
+    <p>Annual Production</p>
+    <p>{{$this->production_site->annual_production}}</p>
+
+    <p>Weekly Production</p>
+    <p>{{$this->production_site->weekly_production}}</p>
+
+    <p>Buffer Tank Size</p>
+    <p>{{$this->production_site->buffer_tank_size}}</p>
 </div>
